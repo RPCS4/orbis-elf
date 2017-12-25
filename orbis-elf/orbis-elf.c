186e521f0756237e90eb487037184c49a3720b40
DH <dh.rpcs3@gmail.com>
2017-12-25 18:00:55 +0300

    Added dump options Print name of known program type

--- orbis-elf/orbis-elf.c
+++ orbis-elf/orbis-elf.c
@@ -79,10 +79,87 @@ const char *orbisElfSymbolBindToString(OrbisElfSymbolBind_t bind)
 	return "<unknown>";
 }
 
-
-void usage(const char *program)
+const char *orbisElfProgramTypeToString(OrbisElfProgramType_t type)
 {
-	printf("usage: %s <path to elf>\n", program);
+	switch (type)
+	{
+	case orbisElfProgramTypeNull: return "null";
+	case orbisElfProgramTypeLoad: return "load";
+	case orbisElfProgramTypeDynamic: return "dynamic";
+	case orbisElfProgramTypeInterp: return "interp";
+	case orbisElfProgramTypeNote: return "note";
+	case orbisElfProgramTypeShlib: return "shlib";
+	case orbisElfProgramTypePhdr: return "phdr";
+	case orbisElfProgramTypeTls: return "tls";
+	case orbisElfProgramTypeSceDynlibData: return "sce_dynlib_data";
+	case orbisElfProgramTypeSceProcParam: return "sce_proc_param";
+	case orbisElfProgramTypeSceRelRo: return "sce_rel_ro";
+	case orbisElfProgramTypeSceComment: return "sce_comment";
+	case orbisElfProgramTypeSceVersion: return "sce_version";
+
+	default:
+		break;
+	}
+
+	return NULL;
+}
+
+enum
+{
+	configDumpHeader = 1 << 0,
+	configDumpSections = 1 << 1,
+	configDumpPrograms = 1 << 2,
+	configDumpImportSymbols = 1 << 3,
+	configDumpExportSymbols = 1 << 4,
+	configDumpRebases = 1 << 5,
+	configDumpSceSymbols = 1 << 6,
+	configDumpSceDynamic = 1 << 7,
+	configDumpTlsInfo = 1 << 8,
+	configDumpImportLibraries = 1 << 9,
+	configDumpImportModules = 1 << 10,
+};
+
+
+static int charToConfigKey(char c)
+{
+	switch (c)
+	{
+	case 'a': return ~0;
+	case 'H': return configDumpHeader;
+	case 'S': return configDumpSections;
+	case 'p': return configDumpPrograms;
+	case 'i': return configDumpImportSymbols;
+	case 'e': return configDumpExportSymbols;
+	case 'r': return configDumpRebases;
+	case 's': return configDumpSceSymbols;
+	case 'd': return configDumpSceDynamic;
+	case 't': return configDumpTlsInfo;
+	case 'l': return configDumpImportLibraries;
+	case 'm': return configDumpImportModules;
+
+	default:
+		break;
+	}
+
+	return 0;
+}
+
+static void usage(const char *program)
+{
+	printf("usage: %s [OPTIONS] <path to elf>\n", program);
+	printf("    OPTIONS:\n");
+	printf("        -a - Dump all (default)\n");
+	printf("        -H - Dump header\n");
+	printf("        -S - Dump sections\n");
+	printf("        -p - Dump programs\n");
+	printf("        -i - Dump import symbols\n");
+	printf("        -e - Dump export symbols\n");
+	printf("        -r - Dump rebases\n");
+	printf("        -s - Dump sce symbols\n");
+	printf("        -d - Dump sce dynamic\n");
+	printf("        -t - Dump TLS info\n");
+	printf("        -l - Dump import libraries\n");
+	printf("        -m - Dump import modules\n");
 }
 
 int main(int argc, const char *argv[])
@@ -93,18 +170,65 @@ int main(int argc, const char *argv[])
 		return 1;
 	}
 
-	struct stat64 fileStat;
-	if (stat64(argv[1], &fileStat) != 0)
+	const char *pathToElf = NULL;
+	int config = 0;
+
+	for (int i = 1; i < argc; ++i)
 	{
-		fprintf(stderr, "File '%s' not found\n", argv[1]);
+		if (argv[i][0] == '-')
+		{
+			for (int j = 1; argv[i][j] != '\0'; ++j)
+			{
+				if (argv[i][j] == 'h')
+				{
+					usage(argv[0]);
+					return 0;
+				}
+				int key = charToConfigKey(argv[i][j]);
+
+				if (key == 0)
+				{
+					usage(argv[0]);
+					return 1;
+				}
+
+				config |= key;
+			}
+		}
+		else if (pathToElf == NULL)
+		{
+			pathToElf = argv[i];
+		}
+		else
+		{
+			usage(argv[0]);
+			return 1;
+		}
+	}
+
+	if (!config)
+	{
+		config = ~0;
+	}
+
+	if (!pathToElf)
+	{
+		usage(argv[0]);
 		return 1;
 	}
 
-	FILE *file = fopen(argv[1], "rb");
+	struct stat64 fileStat;
+	if (stat64(pathToElf, &fileStat) != 0)
+	{
+		fprintf(stderr, "File '%s' not found\n", pathToElf);
+		return 1;
+	}
+
+	FILE *file = fopen(pathToElf, "rb");
 
 	if (!file)
 	{
-		fprintf(stderr, "File '%s' opening error\n", argv[1]);
+		fprintf(stderr, "File '%s' opening error\n", pathToElf);
 		return 1;
 	}
 
@@ -112,7 +236,7 @@ int main(int argc, const char *argv[])
 
 	if (fread(elfData, 1, fileStat.st_size, file) != fileStat.st_size)
 	{
-		fprintf(stderr, "File '%s' reading error\n", argv[1]);
+		fprintf(stderr, "File '%s' reading error\n", pathToElf);
 		free(elfData);
 
 		return 1;
@@ -123,134 +247,195 @@ int main(int argc, const char *argv[])
 
 	if (errorCode != orbisElfErrorCodeOk)
 	{
-		fprintf(stderr, "File '%s' parsing error: %s\n", argv[1], orbisElfErrorCodeToString(errorCode));
+		fprintf(stderr, "File '%s' parsing error: %s\n", pathToElf, orbisElfErrorCodeToString(errorCode));
 		free(elfData);
 		return 0;
 	}
 
-	printf("%s:\n", argv[1]);
-
-	const OrbisElfHeader_t *elfHeader = orbisElfGetHeader(elf);
-	if (elfHeader)
+	if (config & configDumpHeader)
 	{
-		printf("Type: 0x%x - %s\n", elfHeader->type, orbisElfTypeToString(elfHeader->type));
-		printf("Entry point: 0x%" PRIx64 "\n", elfHeader->entry);
-		printf("\n\n");
+		const OrbisElfHeader_t *elfHeader = orbisElfGetHeader(elf);
+
+		if (elfHeader)
+		{
+			printf("Type: 0x%x - %s\n", elfHeader->type, orbisElfTypeToString(elfHeader->type));
+			printf("Entry point: 0x%" PRIx64 "\n", elfHeader->entry);
+			printf("\n\n");
+		}
 	}
 
-	uint16_t programsCount = orbisElfGetProgramsCount(elf);
-	if (programsCount)
+	if (config & configDumpPrograms)
 	{
-		printf("ELF contains %" PRIu16 " programs\n\n", programsCount);
-
+		uint16_t programsCount = orbisElfGetProgramsCount(elf);
 		if (programsCount)
 		{
-			printf("#    type         flags        offset               vaddr                paddr                 memsz                align\n");
+			printf("ELF contains %" PRIu16 " programs\n\n", programsCount);
 
-			for (uint16_t i = 0; i < programsCount; ++i)
+			if (programsCount)
 			{
-				const OrbisElfProgramHeader_t *programHeader = orbisElfProgramGetHeader(orbisElfGetProgram(elf, i));
+				printf("#                Type   Flags      Offset             FileSize           VAddr              PAddr               MemSize            Align\n");
 
-				printf("%-3u  ", i);
+				for (uint16_t i = 0; i < programsCount; ++i)
+				{
+					const OrbisElfProgramHeader_t *programHeader = orbisElfProgramGetHeader(orbisElfGetProgram(elf, i));
 
-				if (programHeader)
-				{
-					printf("0x%08" PRIx32 "   0x%08" PRIx32 "   0x%016" PRIx64 "   0x%016" PRIx64 "   0x%016" PRIx64 "    0x%016" PRIx64 "   0x%016" PRIx64,
-						programHeader->type,
-						programHeader->flags,
-						programHeader->offset,
-						programHeader->vaddr,
-						programHeader->paddr,
-						programHeader->memsz,
-						programHeader->align
-					);
-				}
-				else
-				{
-					printf("<error>");
+					printf("%-3u  ", i);
+
+					if (programHeader)
+					{
+						int needFreeProgramType = 0;
+
+						char *programType = (char *)orbisElfProgramTypeToString(programHeader->type);
+
+						if (!programType)
+						{
+							programType = malloc(32);
+							snprintf(programType, 64, "0x%-8" PRIx32, programHeader->type);
+							needFreeProgramType = 1;
+						}
+
+						printf("% 16s   %-8" PRIx32 "   %-16" PRIx64 "   %-16" PRIx64 "   %-16" PRIx64 "   %-16" PRIx64 "    %-16" PRIx64 "   %-16" PRIx64,
+							programType,
+							programHeader->flags,
+							programHeader->offset,
+							programHeader->filesz,
+							programHeader->vaddr,
+							programHeader->paddr,
+							programHeader->memsz,
+							programHeader->align
+						);
+
+						if (needFreeProgramType)
+						{
+							free(programType);
+						}
+					}
+					else
+					{
+						printf("<error>");
+					}
+
+					printf("\n");
 				}
 
 				printf("\n");
 			}
-
-			printf("\n");
 		}
 	}
 
-	uint16_t sectionsCount = orbisElfGetSectionsCount(elf);
-	if (sectionsCount == orbisElfErrorCodeOk)
+	if (config & configDumpSections)
 	{
-		printf("ELF contains %" PRIu16 " sections\n\n", sectionsCount);
-
-		if (sectionsCount)
+		uint16_t sectionsCount = orbisElfGetSectionsCount(elf);
+		if (sectionsCount == orbisElfErrorCodeOk)
 		{
-			for (uint16_t i = 0; i < sectionsCount; ++i)
+			printf("ELF contains %" PRIu16 " sections\n\n", sectionsCount);
+
+			if (sectionsCount)
 			{
-				OrbisElfSectionHandle_t section = orbisElfGetSection(elf, i);
-				printf("%-3u  %s\n", i, orbisElfSectionGetName(section));
-			}
+				for (uint16_t i = 0; i < sectionsCount; ++i)
+				{
+					OrbisElfSectionHandle_t section = orbisElfGetSection(elf, i);
+					printf("%-3u  %s\n", i, orbisElfSectionGetName(section));
+				}
 
-			printf("\n");
+				printf("\n");
+			}
 		}
 	}
 
-	uint64_t importLibrariesCount = orbisElfGetImportLibrariesCount(elf);
-	if (importLibrariesCount)
+	if (config & configDumpImportLibraries)
 	{
-		for (uint64_t i = 0; i < importLibrariesCount; ++i)
+		uint64_t importLibrariesCount = orbisElfGetImportLibrariesCount(elf);
+		if (importLibrariesCount)
 		{
-			const OrbisElfLibraryInfo_t *info = orbisElfGetImportLibraryInfo(elf, i);
+			for (uint64_t i = 0; i < importLibrariesCount; ++i)
+			{
+				const OrbisElfLibraryInfo_t *info = orbisElfGetImportLibraryInfo(elf, i);
 
-			printf("Import library '%s' version %" PRIu16 ".%" PRIu16 " attributes 0x%" PRIx32 "\n",
+				printf("Import library '%s' version %" PRIu16 ".%" PRIu16 " attributes 0x%" PRIx32 "\n",
 					info->name, info->version >> 8, info->version & 0xff, info->attr);
-		}
-
-		printf("\n\n");
-	}
-
-	uint64_t importModulesCount = orbisElfGetImportModulesCount(elf);
-	if (importModulesCount)
-	{
-		for (uint64_t i = 0; i < importModulesCount; ++i)
-		{
-			const OrbisElfModuleInfo_t *info = orbisElfGetImportModuleInfo(elf, i);
-
-			printf("Import module '%s' version %" PRIu16 ".%" PRIu16 " attributes 0x%" PRIx64 "\n",
-				info->name, info->version >> 8, info->version & 0xff, info->attr);
-		}
-
-		printf("\n\n");
-	}
-
-	uint64_t symbolsCount = orbisElfGetSymbolsCount(elf);
-	if (symbolsCount)
-	{
-		for (uint64_t i = 0; i < symbolsCount; ++i)
-		{
-			const OrbisElfSymbol_t *symbol = orbisElfGetSymbol(elf, i);
-
-			printf("Symbol '");
-
-			if (symbol->library && symbol->module)
-			{
-				printf("%s::%s::%s",
-					symbol->module->name,
-					symbol->library->name,
-					symbol->name
-				);
-			}
-			else
-			{
-				printf("%s", symbol->name);
 			}
 
-			printf("' %s %s at 0x%" PRIx64 " other %u size 0x%" PRIx64 "\n",
-				orbisElfSymbolBindToString(symbol->bind),
-				orbisElfSymbolTypeToString(symbol->type),
-				symbol->header.value, symbol->header.other, symbol->header.size);
+			printf("\n\n");
 		}
+	}
 
-		printf("\n\n");
+	if (config & configDumpImportModules)
+	{
+		uint64_t importModulesCount = orbisElfGetImportModulesCount(elf);
+		if (importModulesCount)
+		{
+			for (uint64_t i = 0; i < importModulesCount; ++i)
+			{
+				const OrbisElfModuleInfo_t *info = orbisElfGetImportModuleInfo(elf, i);
+
+				printf("Import module '%s' version %" PRIu16 ".%" PRIu16 " attributes 0x%" PRIx64 "\n",
+					info->name, info->version >> 8, info->version & 0xff, info->attr);
+			}
+
+			printf("\n\n");
+		}
+	}
+
+	if (config & configDumpSceSymbols)
+	{
+		uint64_t symbolsCount = orbisElfGetSymbolsCount(elf);
+		if (symbolsCount)
+		{
+			for (uint64_t i = 0; i < symbolsCount; ++i)
+			{
+				const OrbisElfSymbol_t *symbol = orbisElfGetSymbol(elf, i);
+
+				printf("Symbol '");
+
+				if (symbol->library && symbol->module)
+				{
+					printf("%s::%s::%s",
+						symbol->module->name,
+						symbol->library->name,
+						symbol->name
+					);
+				}
+				else
+				{
+					printf("%s", symbol->name);
+				}
+
+				printf("' %s %s at 0x%" PRIx64 " other %u size 0x%" PRIx64 "\n",
+					orbisElfSymbolBindToString(symbol->bind),
+					orbisElfSymbolTypeToString(symbol->type),
+					symbol->header.value, symbol->header.other, symbol->header.size);
+			}
+
+			printf("\n\n");
+		}
+	}
+
+	if (config & configDumpImportSymbols)
+	{
+		//TODO
+	}
+
+	if (config & configDumpExportSymbols)
+	{
+		//TODO
+	}
+
+	if (config & configDumpRebases)
+	{
+		//TODO
+	}
+
+	if (config & configDumpSceDynamic)
+	{
+		//TODO
+	}
+
+	if (config & configDumpTlsInfo)
+	{
+		printf("TLS offset: 0x%" PRIx64 "\n", orbisElfGetTlsOffset(elf));
+		printf("TLS size: 0x%" PRIx64 "\n", orbisElfGetTlsSize(elf));
+		printf("TLS align: 0x%" PRIx64 "\n", orbisElfGetTlsAlign(elf));
 	}
 
 	orbisElfDestroy(elf);
